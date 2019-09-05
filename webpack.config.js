7f21003c32845f2cce682bf7febb4f9f353091f0
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2019-09-05 00:22:30 -0700

    add bundle hash

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'index.js'
+    filename: 'index.[hash].js'
   },
   module: {
     rules: [
